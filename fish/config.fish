7fa8544d3708bf9090ceaf9a9a1b75392cc86ce4
DreamMaoMao <2523610504@qq.com>
2024-02-28 11:27:38 +0800

    更新组件设置

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,26 @@
 if status is-interactive
+    # atuin init fish | source
+    source ~/.config/fish/conf.d/atuin.fish
     # Commands to run in interactive sessions can go here
 end
 
 function fish_greeting
 end
 set -g fish_color_autosuggestion purple
-alias api='sudo apt install'
-alias aps='sudo apt search'
+
+
+alias ys='yay -Sy'
+alias yr='yay -R'
 alias s='sudo'
-export PATH=/home/wrq/.nvm/versions/node/v18.14.2/bin:/opt/go/bin:/home/wrq/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/wrq/.local/bin:
-fish_add_path /home/wrq/.spicetify
+alias z='zellij'
+alias y='yazi'
+export PATH=:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/wrq/.local/bin:
+
+# add extra PATH
+fish_add_path /home/wrq/.nvm/versions/node/v18.14.2/bin
+fish_add_path /opt/go/bin
+fish_add_path /home/wrq/.cargo/bin
+
+zoxide init fish --cmd j | source
+
+
