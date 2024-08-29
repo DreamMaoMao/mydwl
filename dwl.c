ffecee9f17715f013f509a3966441be0ebc920dd
DreamMaoMao <2523610504@qq.com>
2024-08-29 19:56:47 +0800

    修复x11应用无限循环的文件描述符事件

--- dwl.c
+++ dwl.c
@@ -1965,7 +1965,6 @@ associatex11(struct wl_listener *listener, void *data)
 {
 	Client *c = wl_container_of(listener, c, associate);
 
-	LISTEN(&client_surface(c)->events.commit, &c->commit, commitnotify);
 	LISTEN(&client_surface(c)->events.map, &c->map, mapnotify);
 	LISTEN(&client_surface(c)->events.unmap, &c->unmap, unmapnotify);
 }
